2ce3f840ddd5e7652385b84e5f47cf0d16884628
Rustuh <lipefel@gmail.com>
2019-08-02 18:19:10 -0300

    Rustuhzin

--- startapp/src/app/app.component.ts
+++ startapp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Russel`s App';
+  title = 'Rustuh Master of Angular';
 }
